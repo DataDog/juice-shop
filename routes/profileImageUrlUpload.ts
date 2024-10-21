16dfdabe94fe405de7df0933cce35923ce618d9b
CarlesDD <carles.capell@datadoghq.com>
2024-10-21 09:31:13 +0200

    Redirect back to profile page once outgoing request has ended

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -14,12 +14,13 @@ const logger = require('../lib/logger')
 
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
+    let imageRequest: Request|null = null
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
-        const imageRequest = request
+        imageRequest = request
           .get(url)
           .on('error', function (err: unknown) {
             UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
@@ -28,15 +29,21 @@ module.exports = function profileImageUrlUpload () {
           .on('response', function (res: Response) {
             if (res.statusCode === 200) {
               const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
+              imageRequest?.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
               UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
             } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
+          })
+          .on('end', function () {
+            res.location(process.env.BASE_PATH + '/profile')
+            res.redirect(process.env.BASE_PATH + '/profile')
           })
       } else {
         next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
       }
     }
-    res.location(process.env.BASE_PATH + '/profile')
-    res.redirect(process.env.BASE_PATH + '/profile')
+    if (!imageRequest) {
+      res.location(process.env.BASE_PATH + '/profile')
+      res.redirect(process.env.BASE_PATH + '/profile')
+    }
   }
 }
